93de5eb55a382f97bd9177e38962c02f03ac3e46
sana <sanaaroojbutt@hotmail.com>
2018-05-14 23:20:38 -0700

    3rd commit

---
+++ CameraCapture (1) - Copy/CameraCapture/CameraCapture/CameraCapture.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.Util;
+using Emgu.CV.CvEnum;
+using System.IO;
+
+namespace CameraCapture
+{
+    public partial class Recording : Form
+    {
+        public string userIdCapture;
+        //declaring global variables
+        private Capture capture;        //takes images from camera as image frames
+        private bool captureInProgress;
+        private HaarCascade haar;
+
+        public Recording()
+        {
+            InitializeComponent();
+        }
+        //------------------------------------------------------------------------------//
+        //Process Frame() below is our user defined function in which we will create an EmguCv 
+        //type image called ImageFrame. capture a frame from camera and allocate it to our 
+        //ImageFrame. then show this image in ourEmguCV imageBox
+        //------------------------------------------------------------------------------//
+        private void ProcessFrame(object sender, EventArgs arg)
+        {
+            Image<Bgr, Byte> ImageFrame = capture.QuerySmallFrame();
+
+            if (ImageFrame != null) {
+
+                Image<Gray, byte> grayframe = ImageFrame.Convert<Gray, byte>();
+                var faces=
+                    grayframe.DetectHaarCascade(haar,1.4,4,Emgu.CV.CvEnum.HAAR_DETECTION_TYPE.DO_CANNY_PRUNING,new Size(25,25))[0];
+                foreach (var face in faces) {
+
+                    ImageFrame.Draw(face.rect, new Bgr(Color.Green), 3);
+                    //byte[] file;
+                    //string path = @"C: \Users\sanaa\Downloads\download.jpg";
+
+                    //using (var stream = new FileStream(path, FileMode.Open, FileAccess.Read))
+                    //{
+                    //    using (var reader = new BinaryReader(stream))
+                    //    {
+                    //        file = reader.ReadBytes((int)stream.Length);
+                    //    }
+                    //}
+                    //database obj = new database();
+                    //MessageBox.Show(userIdCapture);
+                    //obj.saveImage(file, userIdCapture);
+
+                }
+            
+            
+            
+            
+            }
+            CamImgBox.Image = ImageFrame;
+
+            
+        }
+
+        //btnStart_Click() function is the one that handles our "Start!" button' click 
+        //event. it creates a new capture object if its not created already. e.g at first time
+        //starting. once the capture is created, it checks if the capture is still in progress,
+        //if so the
+        private void btnStart_Click_1(object sender, EventArgs e)
+        {
+
+        
+
+            #region if capture is not created, create it now
+            if (capture == null)
+            {
+                try
+                {
+                    capture = new Capture();
+                }
+                catch (NullReferenceException excpt)
+                {
+                    MessageBox.Show(excpt.Message);
+                }
+            }
+            #endregion
+
+            if (capture != null)
+            {
+                if (captureInProgress)
+                {  //if camera is getting frames then stop the capture and set button Text
+                    // "Start" for resuming capture
+                    btnStart.Text = "Start!"; //
+                    record.Hide();
+                    Application.Idle -= ProcessFrame;
+                }
+                else
+                {
+                    //if camera is NOT getting frames then start the capture and set button
+                    // Text to "Stop" for pausing capture
+                    btnStart.Text = "Stop";
+                    record.Show();
+                    Application.Idle += ProcessFrame;
+                }
+
+                captureInProgress = !captureInProgress;
+            }
+        }
+
+        private void ReleaseData()
+        {
+            if (capture != null)
+                capture.Dispose();
+        }
+
+        private void CameraCapture_Load(object sender, EventArgs e)
+        {
+         MessageBox.Show(userIdCapture);
+                haar = new HaarCascade("haarcascade_frontalface_alt2.xml");
+                
+        }
+
+        private void logout_Click(object sender, EventArgs e)
+        {
+            login obj = new login();
+            obj.Show();
+            this.Hide();
+        }
+
+        private void record_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void back_Click(object sender, EventArgs e)
+        {
+            Menu obj = new Menu();
+            obj.Show();
+            this.Hide();
+        }
+
+        private void CamImgBox_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ CameraCapture (1) - Copy/CameraCapture/CameraCapture/sign_up.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace CameraCapture
+{
+    partial class sign_up
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(sign_up));
+            this.logout = new System.Windows.Forms.Button();
+            this.back = new System.Windows.Forms.Button();
+            this.signup = new System.Windows.Forms.Button();
+            this.password = new System.Windows.Forms.TextBox();
+            this.email = new System.Windows.Forms.TextBox();
+            this.ID = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // logout
+            // 
+            this.logout.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.logout.Location = new System.Drawing.Point(473, 1);
+            this.logout.Name = "logout";
+            this.logout.Size = new System.Drawing.Size(75, 23);
+            this.logout.TabIndex = 34;
+            this.logout.Text = "logout";
+            this.logout.UseVisualStyleBackColor = false;
+            this.logout.Click += new System.EventHandler(this.logout_Click);
+            // 
+            // back
+            // 
+            this.back.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.back.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("back.BackgroundImage")));
+            this.back.Location = new System.Drawing.Point(3, 1);
+            this.back.Name = "back";
+            this.back.Size = new System.Drawing.Size(58, 34);
+            this.back.TabIndex = 33;
+            this.back.UseVisualStyleBackColor = false;
+            this.back.Click += new System.EventHandler(this.back_Click);
+            // 
+            // signup
+            // 
+            this.signup.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.signup.Location = new System.Drawing.Point(242, 305);
+            this.signup.Name = "signup";
+            this.signup.Size = new System.Drawing.Size(75, 23);
+            this.signup.TabIndex = 32;
+            this.signup.Text = "Submit";
+            this.signup.UseVisualStyleBackColor = false;
+            this.signup.Click += new System.EventHandler(this.signup_Click);
+            // 
+            // password
+            // 
+            this.password.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.password.Location = new System.Drawing.Point(242, 255);
+            this.password.Name = "password";
+            this.password.Size = new System.Drawing.Size(135, 20);
+            this.password.TabIndex = 31;
+            this.password.UseSystemPasswordChar = true;
+            // 
+            // email
+            // 
+            this.email.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.email.Location = new System.Drawing.Point(242, 207);
+            this.email.Name = "email";
+            this.email.Size = new System.Drawing.Size(135, 20);
+            this.email.TabIndex = 30;
+            // 
+            // ID
+            // 
+            this.ID.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ID.Location = new System.Drawing.Point(242, 160);
+            this.ID.Name = "ID";
+            this.ID.Size = new System.Drawing.Size(135, 20);
+            this.ID.TabIndex = 29;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(113, 255);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(102, 24);
+            this.label5.TabIndex = 28;
+            this.label5.Text = "Password :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(113, 207);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(73, 24);
+            this.label4.TabIndex = 27;
+            this.label4.Text = "E-mail :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(113, 160);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 24);
+            this.label3.TabIndex = 26;
+            this.label3.Text = "ID:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(238, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 24);
+            this.label2.TabIndex = 25;
+            this.label2.Text = "SIGN UP";
+            // 
+            // sign_up
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(549, 341);
+            this.Controls.Add(this.logout);
+            this.Controls.Add(this.back);
+            this.Controls.Add(this.signup);
+            this.Controls.Add(this.password);
+            this.Controls.Add(this.email);
+            this.Controls.Add(this.ID);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Name = "sign_up";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "sign_up";
+            this.Load += new System.EventHandler(this.sign_up_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button logout;
+        private System.Windows.Forms.Button back;
+        private System.Windows.Forms.Button signup;
+        private System.Windows.Forms.TextBox password;
+        private System.Windows.Forms.TextBox email;
+        private System.Windows.Forms.TextBox ID;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+    }
+}
