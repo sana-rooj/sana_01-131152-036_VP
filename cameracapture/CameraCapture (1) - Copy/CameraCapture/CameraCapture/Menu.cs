ecee75ef3651d26fd4cdfc66b5da45a5aaa3385a
sana <sanaaroojbutt@hotmail.com>
2018-05-18 02:59:03 -0700

    my new commit

---
+++ cameracapture/CameraCapture (1) - Copy/CameraCapture/CameraCapture/Menu.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CameraCapture
+{
+    public partial class Menu : Form
+    {
+        public string userIdMenu;
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void secure_Click(object sender, EventArgs e)
+        {
+            Recording obj = new Recording();
+            obj.userIdCapture = userIdMenu;
+            obj.Show();
+            this.Hide();
+        }
+
+        private void logout_Click(object sender, EventArgs e)
+        {
+            login obj = new login();
+            obj.Show();
+            this.Hide();
+        }
+
+        private void back_Click(object sender, EventArgs e)
+        {
+            login obj = new login();
+            obj.Show();
+            this.Hide();
+        }
+
+        private void view_image_Click(object sender, EventArgs e)
+        {
+            images obj = new images();
+            obj.Show();
+            this.Hide();
+        }
+
+        private void Menu_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
---
+++ cameracapture/CameraCapture (1) - Copy/CameraCapture/CameraCapture/images.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CameraCapture
+{
+    public partial class images : Form
+    {
+        public images()
+        {
+            InitializeComponent();
+        }
+
+        private void logout_Click(object sender, EventArgs e)
+        {
+            login obj = new login();
+            obj.Show();
+            this.Hide();
+        }
+
+        private void back_Click(object sender, EventArgs e)
+        {
+            Menu obj = new Menu();
+            obj.Show();
+            this.Hide();
+        }
+
+        private void Search_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Visible=true;
+        }
+    }
+}
